f935f78a20515267c84241a664279215a54bdd87
T. Kim Nguyen <tkimnguyen@users.noreply.github.com>
2026-05-25 15:22:55 -0500

    Add function to export data as CSV

---
+++ export.js
@@ -0,0 +1,10 @@
+function exportCSV(data) {
+  const header = ["Boat","Skipper","Status","Comment","Timestamp"];
+
+  return [header, ...data.map(r =>
+    [r.boat, r.skipper, r.status, r.comment, r.timestamp]
+  )]
+    .map(r => r.join(","))
+    .join("\n");
+}
+``
